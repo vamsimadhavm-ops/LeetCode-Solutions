7eeea6b8debf97efd3030a4b97ba18ccf0a3c052
vamsimadhavm <vamsimadhavm@gmail.com>
2026-06-24 18:44:25 +0530

    Added Two Sum solution

---
+++ Arrays/1-TwoSum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map < int, int > freq;
+
+        for (int i = 0; i < nums.size(); i++){
+            int complement = target - nums[i];
+
+            if(freq.find(complement) !=  freq.end()){
+                return {i, freq[complement]};
+            }
+            freq[nums[i]] = i;
+        }
+        return {};
+    }
+};
